9e723bfb77c2fde34f0c19f867834f43e0b68791
kartavya khandelwal <kartavyakhandelwal1020@gmail.com>
2025-11-02 23:08:51 +0530

    adding new lines

--- index.cpp
+++ index.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main () {
 cout<<"this is a new repositary."<<endl;
-cout<<"this is very exciting."<<endl;
+cout<<"this is very new."<<endl;
 cout<<"this is a good option."<<endl;
    return 0;
 
